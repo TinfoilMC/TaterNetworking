5a36cf746b018922e88c919cf2ef7bffeb804e26
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2020-08-27 17:38:07 -0300

    java 11 netty compressor, idk why i'm doing it, probably negligible performance improvement

---
+++ src/main/java/com/github/creeper123123321/taternetworking/mixin/PacketDeflaterMixin.java
@@ -0,0 +1,55 @@
+package com.github.creeper123123321.taternetworking.mixin;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.PacketDeflater;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.zip.Deflater;
+
+@Mixin(PacketDeflater.class)
+public class PacketDeflaterMixin {
+	@Shadow
+	private int compressionThreshold;
+
+	@Shadow
+	@Final
+	private Deflater deflater;
+
+	@Inject(method = "encode", at = @At("HEAD"), cancellable = true)
+	private void onEncode(ChannelHandlerContext ctx, ByteBuf in, ByteBuf out, CallbackInfo ci) {
+		ci.cancel();
+
+		int nBytes = in.readableBytes();
+		if (nBytes < this.compressionThreshold) {
+			out.writeByte(0); // 0 varint
+			out.writeBytes(in);
+		} else {
+			PacketByteBuf packetByteBuf = new PacketByteBuf(out);
+			packetByteBuf.writeVarInt(nBytes);
+			this.deflater.setInput(in.nioBuffer());
+			this.deflater.finish();
+
+			out.clear();
+			out.ensureWritable(nBytes);
+			ByteBuf buffer = ctx.alloc().buffer(8192, 8192);
+			try {
+				while (!deflater.finished()) {
+					buffer.writerIndex(this.deflater.deflate(buffer.nioBuffer(0, 8192)));
+					out.writeBytes(buffer);
+					buffer.clear();
+				}
+			} finally {
+				buffer.release();
+				in.clear();
+				this.deflater.reset();
+			}
+		}
+	}
+}
---
+++ src/main/java/com/github/creeper123123321/taternetworking/mixin/PacketInflaterMixin.java
@@ -0,0 +1,58 @@
+package com.github.creeper123123321.taternetworking.mixin;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.DecoderException;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.PacketInflater;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.zip.DataFormatException;
+import java.util.zip.Inflater;
+
+@Mixin(PacketInflater.class)
+public class PacketInflaterMixin {
+	@Shadow
+	private int compressionThreshold;
+	@Shadow
+	@Final
+	private Inflater inflater;
+
+	@Inject(at = @At("HEAD"), method = "decode", cancellable = true)
+	private void onDecode(ChannelHandlerContext ctx, ByteBuf in, List<Object> list, CallbackInfo ci) throws DataFormatException {
+		ci.cancel();
+
+		if (in.isReadable()) {
+			PacketByteBuf packetByteBuf = new PacketByteBuf(in);
+			int informedSize = packetByteBuf.readVarInt();
+			if (informedSize == 0) {
+				list.add(packetByteBuf.readRetainedSlice(packetByteBuf.readableBytes()));
+			} else {
+				if (informedSize < this.compressionThreshold) {
+					throw new DecoderException("Badly compressed packet - size of " + informedSize + " is below server threshold of " + this.compressionThreshold);
+				}
+
+				if (informedSize > 2097152) {
+					throw new DecoderException("Badly compressed packet - size of " + informedSize + " is larger than protocol maximum of " + 2097152);
+				}
+
+				this.inflater.setInput(in.nioBuffer());
+				ByteBuf decompressed = ctx.alloc().buffer(informedSize, informedSize);
+				try {
+					decompressed.writerIndex(this.inflater.inflate(decompressed.nioBuffer(0, informedSize)));
+					list.add(decompressed.retain());
+				} finally {
+					decompressed.release();
+					this.inflater.reset();
+					in.clear();
+				}
+			}
+		}
+	}
+}
